3ca4e0688dd299f07e65b460736c079e83653ffc
Blue-Kachina <matt_leering@hotmail.com>
2016-01-29 22:09:54 -0500

    Started using a template for a wizard style site

--- html/index.php
+++ html/index.php
@@ -1,53 +1,221 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: User
- * Date: 2016-01-29
- * Time: 8:49 PM
- */
+
 ?>
 
 
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Helping Developers With Class</title>
+<head>
+	<meta charset="utf-8" />
+	
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<title>Class Creation Wizard</title>
 
-    <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+    <meta name="viewport" content="width=device-width" />
 
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
-  <body>
-    <h1>This tool will allow you to output a basic PHP Class definition for the table you browse</h1>
-    <br/>
-    <br/>
-    <h2>Please provide the following details:</h2>
-    <br/>
+    <link rel="icon" type="image/png" href="assets/img/favicon.ico">
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
+	<link href="assets/css/gsdk-base.css" rel="stylesheet" />
+    
+    <link href="http://netdna.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.css" rel="stylesheet">
+</head>
 
-    <form name="database-details" action="results.php" method="post">
+<body>
+<div class="image-container set-full-height" style="background-image: url('assets/img/wizard.jpg')">
 
-    <div class="label">Server Type</div>
-        <input type="text" name="serverType" id="serverType">
-        <input type="submit">
-    </form>
+    <!--   Big container   -->
+    <div class="container">
+        <div class="row">
+        <div class="col-sm-8 col-sm-offset-2">
+           
+            <!--      Wizard container        -->   
+            <div class="wizard-container"> 
+                <div class="card wizard-card ct-wizard-green" id="wizard">
+                <form action="" method="">
+                <!--        You can switch "ct-wizard-azzure"  with one of the next bright colors: "ct-wizard-blue", "ct-wizard-green", "ct-wizard-orange", "ct-wizard-red"             -->
+                
+                    	<div class="wizard-header">
+                        	<h3>
+                        	   HELPING DEVELOPERS WITH <b>CLASS</b><br/>
+                        	   <small>This will help you generate a class declaration for your table.</small>
+                        	</h3>
+                    	</div>
+                    	<ul>
+                            <li><a href="#server" data-toggle="tab">Server</a></li>
+                            <li><a href="#table" data-toggle="tab">Table</a></li>
+                            <li><a href="#analysis" data-toggle="tab">Analysis</a></li>
+                            <li><a href="#class" data-toggle="tab">Class</a></li>
+                        </ul>
+                        <div class="tab-content">
+                            <div class="tab-pane" id="server">
+                              <div class="row">
+                                  <div class="col-sm-12">
+                                    <h4 class="info-text"> Let's start with the basic database server details</h4>
+                                  </div>
+                                  <div class="col-sm-5 col-sm-offset-1">
+                                      <div class="form-group">
+                                        <label>Type</label>
+                                          <select name="serverType" class="form-control">
+                                              <option disabled="" selected="">- Server Type -</option>
+                                              <option value="Afghanistan"> MySQL </option>
+                                              <option value="Albania"> MS SQL </option>
+                                          </select>
+                                      </div>
+                                  </div>
+                                  <div class="col-sm-5">
+                                       <div class="form-group">
+                                            <label>Server Address</label><br>
+                                            <input type="text" class="form-control" placeholder="IP Address">
+                                          </div>
+                                  </div>
+                                  <div class="col-sm-5 col-sm-offset-1">
+                                      <div class="form-group">
+                                          <label>Username</label>
+                                          <input type="text" class="form-control" placeholder="Username">
+                                      </div>
+                                  </div>
+                                  <div class="col-sm-5">
+                                      <div class="form-group">
+                                          <label>Rent price</label>
+                                          <div class="input-group">
+                                              <input type="text" class="form-control" placeholder="Rent price per day">
+                                              <span class="input-group-addon">$</span>
+                                          </div>
+                                      </div>
+                                  </div>
+                              </div>
+                            </div>
+                            <div class="tab-pane" id="table">
+                                <h4 class="info-text">What type of location do you have? </h4>
+                                <div class="row">
+                                    <div class="col-sm-10 col-sm-offset-1">
+                                        <div class="col-sm-4 col-sm-offset-2">
+                                            <div class="choice" data-toggle="wizard-radio" rel="tooltip" title="Select this option if you have a house.">
+                                                <input type="radio" name="type" value="House">
+                                                <div class="icon">
+                                                    <i class="fa fa-home"></i>
+                                                </div>
+                                                <h6>House</h6>
+                                            </div>
+                                        </div>
+                                        <div class="col-sm-4">
+                                            <div class="choice" data-toggle="wizard-radio" rel="tooltip" title="Select this option if you have an appartment">
+                                                <input type="radio" name="type" value="Appartment">
+                                                <div class="icon">
+                                                    <i class="fa fa-building"></i>
+                                                </div>
+                                                <h6>Appartment</h6>
+                                            </div>
+                                        </div>
+                                        
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="tab-pane" id="analysis">
+                                <h4 class="info-text">Tell us more about facilities. </h4>
+                                <div class="row">
+                                    <div class="col-sm-5 col-sm-offset-1">
+                                      <div class="form-group">
+                                          <label>Your place is good for</label>
+                                          <select class="form-control">
+                                            <option disabled="" selected="">- type -</option>
+                                            <option>Business</option>
+                                            <option>Vacation </option>
+                                            <option>Work</option>
+                                          </select>
+                                      </div>
+                                    </div>
+                                    <div class="col-sm-5">
+                                      <div class="form-group">
+                                          <label>Is air conditioning included ?</label>
+                                          <select class="form-control">
+                                            <option disabled="" selected="">- response -</option>
+                                            <option>Yes</option>
+                                            <option>No </option>
+                                          </select>
+                                      </div>
+                                     </div>
+                                     <div class="col-sm-5 col-sm-offset-1">
+                                      <div class="form-group">
+                                          <label>Does your place have wi-fi?</label>
+                                          <select class="form-control">
+                                            <option disabled="" selected="">- response -</option>
+                                            <option>Yes</option>
+                                            <option>No </option>
+                                          </select>
+                                       </div>
+                                      </div>
+                                      <div class="col-sm-5">
+                                       <div class="form-group">
+                                          <label>Is breakfast included?</label>
+                                          <select class="form-control">
+                                            <option disabled="" selected="">- response -</option>
+                                            <option>Yes</option>
+                                            <option>No </option>
+                                          </select>
+                                       </div>
+                                      </div>
+                                </div>
+                            </div>
+                            <div class="tab-pane" id="class">
+                                <div class="row">
+                                    <h4 class="info-text"> Drop us a small description. </h4>
+                                    <div class="col-sm-6 col-sm-offset-1">
+                                         <div class="form-group">
+                                            <label>Place description</label>
+                                            <textarea class="form-control" placeholder="" rows="9">
+                                                
+                                            </textarea>
+                                          </div>
+                                    </div>
+                                    <div class="col-sm-4">
+                                         <div class="form-group">
+                                            <label>Example</label>
+                                            <p class="description">"The place is really nice. We use it every sunday when we go fishing. It is so awesome."</p>
+                                          </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="wizard-footer">
+                            	<div class="pull-right">
+                                    <input type='button' class='btn btn-next btn-fill btn-success btn-wd btn-sm' name='next' value='Next' />
+                                    <input type='button' class='btn btn-finish btn-fill btn-success btn-wd btn-sm' name='finish' value='Finish' />
+        
+                                </div>
+                                <div class="pull-left">
+                                    <input type='button' class='btn btn-previous btn-fill btn-default btn-wd btn-sm' name='previous' value='Previous' />
+                                </div>
+                                <div class="clearfix"></div>
+                        </div>	
+                    </form>
+                </div>
+            </div> <!-- wizard container -->
+        </div>
+        </div> <!-- row -->
+    </div> <!--  big container -->
+   
+    <div class="footer">
+        <div class="container">
 
+        </div>
+    </div>
+    
+</div>
 
+</body>
 
+    <script src="assets/js/jquery-1.10.2.js" type="text/javascript"></script>
+	<script src="assets/js/bootstrap.min.js" type="text/javascript"></script>
+		
+	<!--   plugins 	 -->
+	<script src="assets/js/jquery.bootstrap.wizard.js" type="text/javascript"></script>
+	
+    <!--  More information about jquery.validate here: http://jqueryvalidation.org/	 -->
+	<script src="assets/js/jquery.validate.min.js"></script>
+	
+	<!--  methods for manipulating the wizard and the validation -->
+	<script src="assets/js/wizard.js"></script>
 
-
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
-  </body>
 </html>
